a49bc811e88f695f27ecaeb94b17804294851d08
Ema <sedyte.ema@gmail.com>
2024-03-22 10:51:07 +0100

    Lazy innlesning

--- kjoretoy/prepp.py
+++ kjoretoy/prepp.py
@@ -10,11 +10,11 @@ STATIC_DATA = THIS_FOLDER / "statiske_data"
 
 def prepp_kjoretoy(inn_fil: Union[str, pathlib.Path]) -> pl.DataFrame:
     logging.info(f"Start prepping data in file {inn_fil}")
-    df = pl.read_parquet(inn_fil)
+    lf = pl.scan_parquet(inn_fil)
 
     # Casting av dato-kolonner.
     # !!!! ERSTATT MED DIN KODE:
-    df = df.with_columns([
+    lf = lf.with_columns([
         pl.col("tekn_reg_f_g_n").cast(str).str.strptime(pl.Date, "%Y%m%d"),
         pl.col("tekn_reg_f_g").cast(str).str.strptime(pl.Date, "%Y%m%d"),
         pl.col("tekn_reg_eier_dato").cast(str).str.strptime(pl.Date, "%Y%m%d"),
@@ -24,12 +24,12 @@ def prepp_kjoretoy(inn_fil: Union[str, pathlib.Path]) -> pl.DataFrame:
 
     # Denne er viktig fordi data er ikke unikt identifisert av kolonnene våre
     # Vi får trøbbel når vi skal gjøre group by senere - noen (forskjellige) biler har identiske data.
-    df = df.with_row_index("id")
+    lf = lf.with_row_index("id")
 
     # Vi leser inn fargekodene
-    fargekode = pl.read_csv(STATIC_DATA / "fargekode.csv", separator=";", dtypes={"kode": str})
+    fargekode = pl.scan_csv(STATIC_DATA / "fargekode.csv", separator=";", dtypes={"kode": str})
 
-    # Legg merke til at tekn_farge i df har 0 først.
+    # Legg merke til at tekn_farge i lf har 0 først.
     # Dette må vi fikse også i fargekode for å kunne joine.
     # Vi vil også bytte navn på "beskrivelse"-kolonna til noe mer fornuftig.
     fargekode = fargekode.with_columns(
@@ -40,19 +40,19 @@ def prepp_kjoretoy(inn_fil: Union[str, pathlib.Path]) -> pl.DataFrame:
 
     # Vi må gjøre en liten jobb for å kunne joine hver fargekode med fargen sin.
     # Først så lager vi en nøstet liste inne i hver kolonne ved å splitte på.
-    df = df.with_columns(pl.col("tekn_farge").str.split(","))
+    lf = lf.with_columns(pl.col("tekn_farge").str.split(","))
 
     # Nå "sprenger" vi denne liste-kolonnen, og lager i stedet for en ny rad pr. listeelement i tekn_farge hvor de andre radene er identiske.
-    df = df.explode("tekn_farge")
+    lf = lf.explode("tekn_farge")
 
     # Med left join så mister vi ingen biler vi ikke vet fargen på.
-    df = df.join(fargekode, left_on="tekn_farge", right_on="kode", how="left")
+    lf = lf.join(fargekode, left_on="tekn_farge", right_on="kode", how="left")
 
     # Vi grupperer på alle kolonner som ikke er tekn farge og farge navn.
     # Se https://docs.pola.rs/user-guide/expressions/column-selections/
     # Vi aggregerer farge_navn og tekn_farge - dette samler bare opp kolonnene som nøstede kolonner (liste-kolonner).
     # Fra liste-kolonnene kan vi joine elementene inne i listene til strenger.
-    df = df.group_by(pl.col("*").exclude("tekn_farge", "farge_navn")).agg(
+    lf = lf.group_by(pl.col("*").exclude("tekn_farge", "farge_navn")).agg(
         pl.col("farge_navn"),
         pl.col("tekn_farge"),
     ).with_columns(
@@ -62,18 +62,18 @@ def prepp_kjoretoy(inn_fil: Union[str, pathlib.Path]) -> pl.DataFrame:
 
     # Vi lager en egen elbil-kolonne
     # !!!! ERSTATT MED DIN KODE:
-    df = df.with_columns(
+    lf = lf.with_columns(
         (pl.col("tekn_drivstoff") == "Elektrisk").alias("elbil")
     )
 
     # Vi vil også ha inn skriftlig beskrivelse av bilmerket
-    merkekode = pl.read_csv(STATIC_DATA / "merkekode.csv", separator=";").rename(
+    merkekode = pl.scan_csv(STATIC_DATA / "merkekode.csv", separator=";").rename(
         {"navn": "merke_navn"}
     )
-    df = df.join(merkekode, left_on="tekn_merke", right_on="kode", how="left")
+    lf = lf.join(merkekode, left_on="tekn_merke", right_on="kode", how="left")
 
     # Vi henter ut kolonnene vi trenger
-    df = df.select(
+    lf = lf.select(
         "id",
         "tekn_reg_f_g_n",
         "tekn_reg_eier_dato",
@@ -85,4 +85,4 @@ def prepp_kjoretoy(inn_fil: Union[str, pathlib.Path]) -> pl.DataFrame:
         "farge_navn",
         "elbil")
     logging.info(f"Finished prepping {inn_fil}")
-    return df
+    return lf.collect()
